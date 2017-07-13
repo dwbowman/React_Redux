53f5ac5dc2409a9970d3f175e310d96de4992707
Dylan Bowman <n644f787@pi.atai.lan>
2017-07-13 10:58:03 -0500

    first commit

---
+++ BookApp/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Book1', pages: 101 },
+    { title: 'Book2', pages: 22 },
+    { title: 'Book3', pages: 789 },
+    { title: 'Book4', pages: 15 }
+  ]
+}
